95ae8e49d33ff0a1adb0602384dd41c4da756d14
OKONG <kongwf5813@qq.com>
2020-10-11 22:04:20 +0800

    10-11

--- src/main/java/com/owen/algorithm/DynamicProgramming.java
+++ src/main/java/com/owen/algorithm/DynamicProgramming.java
@@ -307,6 +307,24 @@ public class DynamicProgramming {
         return maxSide * maxSide;
     }
 
+    //[300]最长上升子序列
+    public static int lengthOfLIS(int[] nums) {
+        int size = nums.length;
+        if (size == 0) return 0;
+        int[] dp = new int[size];
+        int length = 1;
+        for (int i = 0; i < size; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            length = Math.max(length, dp[i]);
+        }
+        return length;
+    }
+
     public static void main(String[] args) {
 //        [5]最长回文子串
 //        System.out.println(longestPalindrome("a"));
