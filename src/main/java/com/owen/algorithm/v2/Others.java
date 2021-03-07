7e8d21c1a1614e9b7f78f514f87b96c3c5cbe225
OKONG <kongwf5813@qq.com>
2021-03-07 22:20:19 +0800

    3-7

--- src/main/java/com/owen/algorithm/v2/Others.java
+++ src/main/java/com/owen/algorithm/v2/Others.java
@@ -2,12 +2,25 @@ package com.owen.algorithm.v2;
 
 import com.owen.algorithm.LinkList.ListNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Others {
 
-    //    [2]
+    //[1].两数之和
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int index = 0; index < nums.length; index++) {
+
+            if (hash.containsKey(target - nums[index])) {
+                return new int[]{hash.get(target - nums[index]), index};
+            }
+            hash.put(nums[index], index);
+        }
+        return new int[0];
+    }
+
+
+    //[2]两数相加
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(-1);
         ListNode cur = dummyHead;
@@ -129,7 +142,7 @@ public class Others {
     //[13].罗马数转整数
     public static int romanToInt(String s) {
         int pre = getRomanNumber(s.charAt(0)), sum = 0;
-        for (int i = 1; i< s.length(); i++) {
+        for (int i = 1; i < s.length(); i++) {
             int val = getRomanNumber(s.charAt(i));
             if (pre < val) {
                 sum -= pre;
@@ -144,14 +157,144 @@ public class Others {
 
     private static int getRomanNumber(char ch) {
         switch (ch) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D':return 500;
-            case 'M': return 1000;
-            default: return 0;
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    //[14].最长公共前缀
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String res = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            res = longestCommonPrefix(res, strs[i]);
+            if (res.length() == 0) {
+                return "";
+            }
+        }
+        return res;
+    }
+
+    private static String longestCommonPrefix(String first, String second) {
+        int index = 0, minLen = Math.min(first.length(), second.length());
+        while (index < minLen && (first.charAt(index) == second.charAt(index))) {
+            index++;
+        }
+        return first.substring(0, index);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> pos = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; ) {
+            int target = 0 - nums[i];
+            List<List<Integer>> sums = twoSums(nums, i + 1, target);
+            if (sums.size() > 0) {
+                for (List<Integer> sum : sums) {
+                    sum.add(nums[i]);
+                    pos.add(sum);
+                }
+            }
+            i++;
+            while (i < nums.length - 2 && nums[i - 1] == nums[i]) {
+                i++;
+            }
+        }
+        return pos;
+    }
+
+    private static List<List<Integer>> twoSums(int[] nums, int start, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            int leftOne = nums[left];
+            int rightOne = nums[right];
+            int sum = leftOne + rightOne;
+            if (sum == target) {
+                List<Integer> group = new ArrayList<>();
+                group.add(leftOne);
+                group.add(rightOne);
+                res.add(group);
+                while (left < right && nums[left] == leftOne) left++;
+                while (left < right && nums[right] == rightOne) right--;
+            } else if (sum < target) {
+                while (left < right && nums[left] == leftOne) left++;
+            } else {
+                while (left < right && nums[right] == rightOne) right--;
+            }
+        }
+        return res;
+    }
+
+    //[16].最接近的三数之和
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; ) {
+            int num = nums[i];
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int rightOne = nums[right], leftOne = nums[left];
+                int sum = num + leftOne + rightOne;
+                if (sum == target) {
+                    return sum;
+                }
+
+                if (Math.abs(target - sum) < Math.abs(target - res)) {
+                    res = sum;
+                }
+                if (sum > target) {
+                    while (left < right && nums[right] == rightOne) right--;
+                } else {
+                    while (left < right && nums[left] == leftOne) left++;
+                }
+            }
+            i++;
+            while (i < nums.length - 2 && nums[i - 1] == nums[i]) i++;
+        }
+        return res;
+    }
+
+
+    //[17].电话号码的字母组合
+    public static List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if (digits.length() == 0) return res;
+        String[] strings = new String[]{"-", "-", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        dfsForLetter(res, new StringBuilder(), digits, 0, strings);
+        return res;
+    }
+
+
+    private static void dfsForLetter(List<String> res, StringBuilder select, String digits, int index, String[] strings) {
+        if (digits.length() == select.length()) {
+            res.add(select.toString());
+            return;
+        }
+
+        String string = strings[digits.charAt(index) - '0'];
+        for (int i = 0; i < string.length(); i++) {
+            select.append(string.charAt(i));
+
+            dfsForLetter(res, select, digits, index + 1, strings);
+            select.deleteCharAt(select.length() - 1);
         }
     }
 
@@ -175,7 +318,7 @@ public class Others {
         System.out.println(reverse(Integer.MAX_VALUE));
         System.out.println(reverse(123));
 
-//        System.out.println(myAtoi("--"));
+
         System.out.println(myAtoi("-2147483649"));
         System.out.println(myAtoi("-91283472332"));
         System.out.println(myAtoi("   -42"));
@@ -189,5 +332,19 @@ public class Others {
 
         System.out.println(romanToInt("MMMCMXCIX"));
         System.out.println(romanToInt("I"));
+
+
+        System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+
+
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(threeSum(new int[]{0, 0, 0, 0, 0, 0, 0}));
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4}));
+
+        System.out.println(threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+
+        System.out.println(letterCombinations("23"));
+        System.out.println(letterCombinations("22"));
     }
 }
