f24bd11cbb181e7cda4ad7880d88807c9a520cc0
kongwenfei <kongwf5813@qq.com>
2021-05-07 19:38:05 +0800

    5-7

--- src/main/java/com/owen/algorithm/v2/Others.java
+++ src/main/java/com/owen/algorithm/v2/Others.java
@@ -2748,6 +2748,19 @@ public class Others {
         return level;
     }
 
+    //[226]
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+
     public static void main(String[] args) {
 //        ListNode f = new ListNode(2);
 //        f.next = new ListNode(4);
